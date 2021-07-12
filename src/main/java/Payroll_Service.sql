e7b84298c1ce252cbd2f165e3fe10f63a9d98afd
Shweta2105 <sghadi21@gmail.com>
2021-07-12 07:34:47 +0530

    UC1

---
+++ src/main/java/Payroll_Service.sql
@@ -0,0 +1,24 @@
+/*MySQL Database
+    Employee Payroll service
+ */
+
+//UC 1
+mysql> create DATABASE payroll_service;
+Query OK, 1 row affected (0.83 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| classicmodels      |
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sys                |
++--------------------+
+6 rows in set (0.73 sec)
+
+mysql> use payroll_service;
+Database changed
+
