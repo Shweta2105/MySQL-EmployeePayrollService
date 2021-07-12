fffe52a4768dba0c0012a0e42b03930961a13e1d
Shweta2105 <sghadi21@gmail.com>
2021-07-12 07:37:10 +0530

    UC2 employee payroll table created

--- src/main/java/Payroll_Service.sql
+++ src/main/java/Payroll_Service.sql
@@ -21,4 +21,26 @@ mysql> show databases;
 
 mysql> use payroll_service;
 Database changed
+//UC 2
+
+mysql> use payroll_service;
+Database changed
+mysql> create table employee_payroll(
+    -> id int NOT NULL auto_increment,
+    -> name varchar(50) NOT NULL,
+    -> salary int NOT NULL,
+    -> start date,
+    -> primary key (id));
+Query OK, 0 rows affected (8.36 sec)
+
+mysql> desc employee_payroll;
++--------+-------------+------+-----+---------+----------------+
+| Field  | Type        | Null | Key | Default | Extra          |
++--------+-------------+------+-----+---------+----------------+
+| id     | int         | NO   | PRI | NULL    | auto_increment |
+| name   | varchar(50) | NO   |     | NULL    |                |
+| salary | int         | NO   |     | NULL    |                |
+| start  | date        | YES  |     | NULL    |                |
++--------+-------------+------+-----+---------+----------------+
+4 rows in set (0.84 sec)
 
